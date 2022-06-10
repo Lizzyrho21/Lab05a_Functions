f35b9729dbbbef3203eaceb253abd82fe0641700
nunumarie3000 <96929239+NunuMarie3000@users.noreply.github.com>
2022-06-10 11:58:50 -0500

    added sum bracnh and finished code

--- js/app.js
+++ js/app.js
@@ -9,11 +9,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+// declared my array variable
+  let newsumArr = [];
+  // set the total from my paramters
+  let total = a + b;
+  // created a string variable
+  let newString = 'The sum of 4 and 7 is 11.';
+  // push my total and string inside of the array
+  newsumArr.push(total, newString);
+  // returned the array
+  return newsumArr;
 
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
